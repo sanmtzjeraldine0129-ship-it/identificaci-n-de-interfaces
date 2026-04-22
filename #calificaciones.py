40d4ec8418e3ce5508975cb7e44bdc2f27e045e2
jeral <sanmtzjeraldine0129@gmail.com>
2026-04-21 18:47:10 -0600

    Add files via upload

---
+++ #calificaciones.py
@@ -0,0 +1,19 @@
+#calificaciones
+#monserrat jeraldine
+
+ 
+nota1 = 8
+nota2 = 6
+nota3 = 9
+nota4 = 7
+nota5 = 10
+ 
+promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+ 
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Nota 3:", nota3)
+print("Nota 4:", nota4)
+print("Nota 5:", nota5)
+ 
+print(f"El promedio es: {promedio:.2f}")
