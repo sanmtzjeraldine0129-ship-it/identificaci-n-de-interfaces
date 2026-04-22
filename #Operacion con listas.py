55ab9413fb50412c5b308f0c5f34153621993616
jeral <sanmtzjeraldine0129@gmail.com>
2026-04-21 18:46:17 -0600

    Add files via upload

---
+++ #Operacion con listas.py
@@ -0,0 +1,9 @@
+#promedio
+#Monserrat Jeraldine
+
+numeros = [45, 12, 17, 78, 3, 56, 89, 23, 67, 1, 34]
+
+total = sum(numeros)
+promedio = sum(numeros) / 1en(numeros)
+print(f'suna: {total}')
+print(f'promedio:  {promedio:2f}')
