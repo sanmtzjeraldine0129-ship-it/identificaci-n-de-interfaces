9cb10fde87662989c5f7829aa330cd9a22e7f67b
jeral <sanmtzjeraldine0129@gmail.com>
2026-04-22 23:32:31 -0600

    Add files via upload

---
+++ #Funciones y procedimientos.py
@@ -0,0 +1,8 @@
+#Funciones y procedimientos 
+#monserrat Jeraldine 
+#funcion reciba 2 numeros y devuelva su multiplicacion
+
+def multiplicar (a,b):
+    return a * b
+resultado = multiplicar (4, 5)
+print(resultado)
