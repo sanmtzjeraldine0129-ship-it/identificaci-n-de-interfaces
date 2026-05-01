5c737b6b88242bba07233ccf949cf578cf77e2e8
jeral <sanmtzjeraldine0129@gmail.com>
2026-04-30 20:24:27 -0600

    Add files via upload

---
+++ #Monserrat Jeraldine.py
@@ -0,0 +1,9 @@
+#Monserrat Jeraldine
+#Variables No-Numericas
+
+nombre_usuario = "Jera" #Texto (str)
+Cantidad_de_pizzas = 2 #Numero (int)
+esta_pagado = True #Booleano (bool)
+direccion_envio = "Calle avenida 177" #Texto (str)
+precio_total = 155.50 #Numero (float)
+
