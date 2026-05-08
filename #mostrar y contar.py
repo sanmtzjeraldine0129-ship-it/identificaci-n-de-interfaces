3fa105b92ce920f16bf4d580eeeeda968d992a78
jeral <sanmtzjeraldine0129@gmail.com>
2026-05-07 20:37:59 -0600

    Add files via upload

---
+++ #mostrar y contar.py
@@ -0,0 +1,10 @@
+#mostrar y contar
+#Jeraldine Sanchez
+
+contador = 0
+
+
+for i in range(1, 51):
+    if i % 2 == 0:
+        contador = contador + 1
+print("Cantidad de números pares:", contador)
